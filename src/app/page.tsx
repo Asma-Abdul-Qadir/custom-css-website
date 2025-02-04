630c6e9d7feac94b6505d6684c31f2e20c52857d
Asma-Abdul-Qadir <asmagigani786@gmail.com>
2025-02-05 01:04:55 +0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import styles from './home.module.css';
-import Link from 'next/link';
+
 
 const Home: React.FC = () => {
   return (
