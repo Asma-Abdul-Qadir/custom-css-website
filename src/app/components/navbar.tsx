51ce3ffc255fea3125381b339b7438501148286c
Asma-Abdul-Qadir <asmagigani786@gmail.com>
2025-02-08 03:41:03 +0500

    update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,19 +1,45 @@
+'use client'
 import Link from 'next/link';
+import { useState } from 'react';
 import styles from '../components/navbar.module.css';
 
 const Navbar: React.FC = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const handleToggle = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Close the mobile menu when a link is clicked
+  const handleLinkClick = () => {
+    setMobileMenuOpen(false);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>TravelTales</div>
-      <ul className={styles.navLinks}>
+      <button className={styles.menuToggler} onClick={handleToggle}>
+        &#9776; {/* Unicode hamburger icon */}
+      </button>
+      <ul
+        className={`${styles.navLinks} ${
+          isMobileMenuOpen ? styles.mobileMenuOpen : ''
+        }`}
+      >
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/" onClick={handleLinkClick}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link href="/destinations">Destinations</Link>
+          <Link href="/destinations" onClick={handleLinkClick}>
+            Destinations
+          </Link>
         </li>
         <li>
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact" onClick={handleLinkClick}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
